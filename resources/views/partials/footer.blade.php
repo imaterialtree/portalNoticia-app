3965c2da49561cd1de0ebba8f12a8e0caf928edd
Naoki Miura <naokirafael.miura@gmail.com>
2024-08-04 17:14:17 -0300

    laravel 11 com docker projeto vazio de laravel 11 com docker configurado

--- resources/views/partials/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<footer class="bg-dark text-white text-center p-3">
-    <div class="container">
-        <p>&copy; 2024 Portal Noticia. Todos os direitos reservados.</p>
-    </div>
-</footer>
--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,7 @@
 <?php
 
-use App\Http\Controllers\NoticiaController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', [NoticiaController::class, 'home'])->name('home');
-Route::resource('noticias', NoticiaController::class);
-
-Route::get('/dashboard', [NoticiaController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
-//Route::get('/noticias/{noticia}', [NoticiaController::class, 'show'])->name('noticias.show');
-Route::put('/noticias/{noticia}', [NoticiaController::class, 'update'])->name('noticias.update');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::get('/', function () {
+    return view('welcome');
 });
-
-require __DIR__.'/auth.php';
