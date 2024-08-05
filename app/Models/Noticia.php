3b08808f12905bcf68e21e4ffd0646287802a368
Naoki Miura <naokirafael.miura@gmail.com>
2024-08-05 09:46:03 -0300

    config scout with algolia

--- app/Models/Noticia.php
+++ app/Models/Noticia.php
@@ -6,9 +6,12 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
+use Laravel\Scout\Searchable;
 
 class Noticia extends Model
 {
+    use Searchable;
+
     use HasFactory;
     protected $fillable = [
         'titulo',
@@ -25,7 +28,7 @@ class Noticia extends Model
         }
     }
 
-    public static function filter(array $filters)
+    public static function scopeFilter(Builder $query, array $filters)
     {
         $query = null;
         if ($titulo = $filters['titulo'] ?? false) {
@@ -33,9 +36,18 @@ class Noticia extends Model
         }
 
         if ($descricao = $filters['descricao'] ?? false) {
-
+            $query = self::where('descricao', $descricao);
         }
         return $query;
     }
 
+    // laravel/scout
+    public function toSearchableArray()
+    {
+        return [
+            'id' => $this->id,
+            'titulo' => $this->titulo,
+            'descricao' => $this->descricao,
+        ];
+    }
 }
